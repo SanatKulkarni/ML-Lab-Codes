0791639a0ce23d8bc3363e2768da87f37ab48daf
Sanat Kulkarni <87092449+SanatKulkarni@users.noreply.github.com>
2024-11-22 00:46:44 +0530

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -1,14 +1,16 @@
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
-data = pd.read_csv('./irisDataset.csv')
-numeric_data = data.select_dtypes(include=[float, int])
-correlation_matrix = numeric_data.corr()
-print(correlation_matrix)
-correlation_matrix_unstacked = correlation_matrix.abs().unstack()
-sorted_correlation = correlation_matrix_unstacked.sort_values(ascending=False)
-sorted_correlation = sorted_correlation[sorted_correlation != 1]
-strongest_pair = sorted_correlation.idxmax()
-sns.pairplot(data, hue="variety", markers=["o", "s", "D"])
-plt.show()
-print(f"\nThe two attributes showing the strongest linear relationship are: {strongest_pair}")
+
+data=pd.read_csv('/content/irisDataset.csv')
+
+numeric_data=data.select_dtypes(include=[float,int])
+correlation_matrix=numeric_data.corr()
+
+correlation_matrix_unstacked=correlation_matrix.unstack()
+sorted_correlation=correlation_matrix_unstacked.sort_values(ascending=False)
+sorted_correlation=sorted_correlation[sorted_correlation!=1]
+print(sorted_correlation)
+
+strongest_pair=sorted_correlation.idxmax()
+
+print(f"Attributes strongest pair:{strongest_pair}")
