398d3adaf861285ebe4158c79a65718f5d53e7e5
Sanat Kulkarni <87092449+SanatKulkarni@users.noreply.github.com>
2024-11-22 01:22:27 +0530

    Update simpleLinearRegression.py

--- simpleLinearRegression.py
+++ simpleLinearRegression.py
@@ -1,26 +1,27 @@
 def mean(values):
-    return sum(values) / len(values)
+  return sum(values)/len(values)
 
 def sl_regressor(x,y):
-    n=len(x)
-    mean_x=mean(x)
-    mean_y=mean(y)
-    numerator=sum((x[i]-mean_x)*(y[i]-mean_y) for i in range(n))
-    denominator=sum((x[i]-mean_x)**2 for i in range(n))
-    b1=numerator/denominator
-    b0=mean_y-b1*mean_x
+  n=len(x)
+  mean_x=mean(x)
+  mean_y=mean(y)
 
-    print(f"Slope:{b1}")
-    print(f"Intercept:{b0}")
+  numerator=sum((x[i]-mean_x)*(y[i]-mean_y) for i in range(n))
+  denominator=sum((x[i]-mean_x)**2 for i in range(n))
 
-    return b1,b0
+  b1=numerator/denominator
+  b0=mean_y-b1*mean_x
 
-x=list(map(float,input("Enter values for x:").split(',')))
-y=list(map(float,input("Enter values for y:").split(',')))
+  print(f"Slope:{b1}")
+  print(f"Intercept:{b0}")
+  print(f"Line equation:\n")
+  print(f"y={b0}+{b1}*x")
 
-print(f"x:{x}")
-print(f"y:{y}")
+  return b1,b0
 
+x=list(map(float,input("enter x values:").split(',')))
+y=list(map(float,input("enter y values:").split(',')))
+
+print(x)
+print(y)
 b1,b0=sl_regressor(x,y)
-print(f"Slope:{b1}")
-print(f"Intercept:{b0}")
